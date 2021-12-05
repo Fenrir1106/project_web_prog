bbf505854e82931775a397fc8f53a00fca7f738c
Agung Yuwono Sugiyono <agungabadi9999@gmail.com>
2021-12-05 12:04:54 +0700

    database migratio done

---
+++ database/migrations/2021_12_04_101946_create_transaction_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->increments("id");
+            $table->unsignedInteger("header_id");
+            $table->unsignedInteger("product_id");
+            $table->integer("quantity");
+            $table->foreign("header_id")->references("id")->on("transaction_headers");
+            $table->foreign("product_id")->references("id")->on("products");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+}
